b5de02ca01e6efb84554b6b826c6a3054200f0c0
Patevah <patrik.vahala@netikka.fi>
2024-09-30 16:08:22 +0300

    toinen vienti

---
+++ noppa.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+from random import randint
+
+class NoppaSovellus:
+    def __init__(self, ikkuna):
+        self.ikkuna = ikkuna
+        self.ikkuna.title("Noppasovellus")
+
+        self.tulos_label = tk.Label(ikkuna, text="Tulos näkyy tässä")
+        self.tulos_label.pack(pady=10)
+
+        self.heitto_painike = tk.Button(ikkuna, text="Heitä noppaa", command=self.arvo_luku)
+        self.heitto_painike.pack()
+
+    def arvo_luku(self):
+        tulos = randint(1, 6)
+        self.tulos_label.config(text=f"Heitto: {tulos}")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    sovellus = NoppaSovellus(root)
+    root.mainloop()
